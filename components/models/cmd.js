7bad57af7b8f865066ad2f7f6482519e7528e61b
zlh-debug <3530766280@qq.com>
2023-03-09 17:33:45 +0800

    rm cmd.js

--- components/models/cmd.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch';
-import request from 'request';
-import fs from 'fs';
-import {segment} from 'oicq';
-
-try{print(67)
-Bot.logger.mark('命令执行成功');}catch(e){ev.reply(e.toString());}
